5ea7737b30c6bd2078c92b2ee124c0764950c16d
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2017-11-12 12:24:41 -0200

    Dando os toques finais para o Demo

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,24 +1,3 @@
-var mensagens = [
-    {
-        mensagem: "Por favor, Mostre para a câmera algum documento com foto",
-        enviador: "bot"
-    }, {
-        mensagem: "o documento do usuário",
-        enviador: "usuario"
-    }, {
-        mensagem: "Ótimo, Thiago. Agora preciso confirmar sua carteira de convênio e seu código de cliente, mande uma foto por favor?",
-        enviador: "bot"
-    }, {
-        mensagem: "A foto do usuário",
-        enviador: "usuario"
-    }, {
-        mensagem: "Bom Thiago muito obrigado pela confirmação dirija-se ao 3º andar sala 15. Obrigado pela preferência",
-        enviador: "bot"
-    }
-];
-
-mensagens.forEach(criarMensagem);
-
 function criarMensagem(item) {
     var mensagem = document.createElement("div");
     mensagem.classList = "mensagem mensagem-" + item.enviador;
@@ -36,17 +15,23 @@ function criarMensagem(item) {
 }
 
 document.getElementById('inputUsuario').addEventListener('change', function(ev) {
+
+    criarMensagem({
+        enviador: 'usuario',
+        mensagem: ev.target.value
+    });
+    
     var client = new XMLHttpRequest();
     client.open("GET", "http://9.232.16.79:8081/api", true);
     client.setRequestHeader("Content-type", "application/json");
     client.setRequestHeader("TextoUsuario", ev.target.value);
     client.send();
 
+    ev.target.value = '';
+
     client.onreadystatechange = function() {
         if (client.readyState == XMLHttpRequest.DONE) {
             var response = JSON.parse(client.responseText);
-            console.log(response);
-            console.log(response.message);
             if(response.success != false) {
                 criarMensagem({
                     enviador: 'bot',
@@ -57,5 +42,4 @@ document.getElementById('inputUsuario').addEventListener('change', function(ev) 
             }
         }
     }
-
 });
